a59d4f9380838202e5bbb860397eae83739442b5
davjdo <davido_204@hotmail.com>
2018-09-25 03:35:50 +0200

    add Clients, Dashboard, Sidebar components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppNavbar from './components/layout/AppNavbar';
+import Dashboard from './components/layout/Dashboard';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,9 @@ class App extends Component {
       <div className="App">
         <AppNavbar />
         <div className="container">
-          <h1>hello</h1>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+          </Switch>
         </div>
       </div>
     );
