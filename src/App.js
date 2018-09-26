01f2ce404a9397ee64b436a46af4dd17ec56c911
davjdo <davido_204@hotmail.com>
2018-09-26 02:37:03 +0200

    add AddClient component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
+import AddClient from './components/clients/AddClient';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="container">
           <Switch>
             <Route exact path="/" component={Dashboard} />
+            <Route exact path="/client/add" component={AddClient} />
           </Switch>
         </div>
       </div>
